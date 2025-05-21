b2e5f5a4b9ba405be48a3e7ccb7a2efe54095f2b
nvmith <156288478+nvmith@users.noreply.github.com>
2025-05-21 17:13:00 +0900

    Update solve.cpp

--- 005/solve.cpp
+++ 005/solve.cpp
@@ -1,1 +1,45 @@
-// 
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+map<int,int> factorMap;
+
+map<int,int> fac(int n)
+{
+    map<int, int> f;
+    
+    for (ll i = 2; i * i <= n; ++i) {
+        while (n % i == 0) {
+            f[i]++;
+            n /= i;
+        }
+    }
+    if(n>1) f[n]++;
+    
+    return f;
+}
+
+int main()
+{
+    for(int i=2;i<=20;i++)
+    {
+        map<int, int> current = fac(i);
+
+        for (auto p : current) {
+            int num = p.first;
+            int count = p.second;
+
+            if (factorMap[num] < count)
+                factorMap[num] = count;
+        }
+    }
+    
+    ll result = 1;
+    
+    for(auto a : factorMap)
+        for(int i=0;i<a.second;i++)
+            result *= a.first;
+    
+    cout << result;
+}
