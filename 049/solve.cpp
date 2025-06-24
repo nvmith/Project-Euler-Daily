9c4dcbdc4ab10674032afd140b9d956758b90712
nvmith <156288478+nvmith@users.noreply.github.com>
2025-06-24 17:07:09 +0900

    fix: 조건 추가
    - 문제의 답을 구하지 못한 코드여서 조건을 추가해 해결

--- 049/solve.cpp
+++ 049/solve.cpp
@@ -15,30 +15,33 @@ int main()
     cin.tie(0);
     
     vector<int> p;
-    for (int i = 1000; i <= 9999; i++) {
+    for (int i = 1000; i <= 9999; i++)
         if (isPrime(i)) p.push_back(i);
-    }
 
-    for (int num : p) {
+    for (int num : p) 
+    {
         string s = to_string(num);
         sort(s.begin(), s.end());
 
         set<int> primes;
         do {
             int tmp = stoi(s);
-            if (isPrime(tmp)) primes.insert(tmp);
+            if (isPrime(tmp) && tmp >= 1000) primes.insert(tmp);
         } while (next_permutation(s.begin(), s.end()));
 
         if (primes.size() < 3) continue;
 
         vector<int> arr(primes.begin(), primes.end());
 
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = i+1; j < arr.size(); j++) {
+        for (int i = 0; i < arr.size(); i++) 
+        {
+            for (int j = i+1; j < arr.size(); j++) 
+            {
                 int a = arr[i], b = arr[j];
                 int diff = b - a;
                 int c = b + diff;
-                if (primes.count(c)) {
+                if (primes.count(c)) 
+                {
                     if (a == 1487 && b == 4817) continue;
                     cout << a << b << c;
                     return 0;
